15fe65559d3892136fe73e7bc4e819b874a2c89e
alixcaroline <alixcaroline13@gmail.com>
2023-08-20 21:25:41 +0200

    mark done, not done and edit task

--- todo/urls.py
+++ todo/urls.py
@@ -3,5 +3,12 @@ from . import views
 
 
 urlpatterns = [
+    # Add task
     path("addTask/", views.addTask, name="addTask"),
+    # Mark task as done
+    path("mark_as_done/<int:pk>/", views.mark_as_done, name="mark_as_done"),
+    # Mark task as not done
+    path("mark_as_not_done/<int:pk>/", views.mark_as_not_done, name="mark_as_not_done"),
+    # Edit task
+    path("edit_task/<int:pk>/", views.edit_task, name="edit_task"),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import render, redirect
+from django.shortcuts import get_object_or_404, render, redirect
 from .models import Task
 
 
@@ -8,3 +8,31 @@ def addTask(request):
     task = request.POST["task"]
     Task.objects.create(task=task)
     return redirect("home")
+
+
+def mark_as_done(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.is_completed = True
+    task.save()
+    return redirect("home")
+
+
+def mark_as_not_done(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.is_completed = False
+    task.save()
+    return redirect("home")
+
+
+def edit_task(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    if request.method == "POST":
+        updated_task = request.POST["task"]
+        task.task = updated_task
+        task.save()
+        return redirect("home")
+    else:
+        context = {
+            "task": task,
+        }
+        return render(request, "edit_task.html", context)
